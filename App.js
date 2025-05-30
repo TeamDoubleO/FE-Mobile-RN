0d25645750d57f0fddd2ffffccda77c03c8e4b00
zzzzzisu <kimjisu4939@gmail.com>
2025-05-30 14:54:35 +0900

    KW-540/feat: FCM(Firebase Cloud Messaging) 기본 설정

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
-import AppNavigator from './src/navigations/AppNavigator';
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppNavigator from './src/navigations/AppNavigator';
+// import { firebase } from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    // Firebase App이 정상적으로 초기화되었는지 확인
+    // console.log('Firebase App Name:', firebase.app().name); // [DEFAULT]가 찍히면 정상
+    // FCM 토큰 받아오기
+    const getToken = async () => {
+      try {
+        const fcmToken = await messaging().getToken();
+        console.log('FCM Token:', fcmToken);
+      } catch (error) {
+        console.log('Error getting FCM token:', error);
+      }
+    };
+
+    getToken();
+  }, []);
   return (
     <SafeAreaProvider>
       <AppNavigator />
