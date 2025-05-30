7b0f65b248697978bdec1910fe5e0768fdea6b36
zzzzzisu <kimjisu4939@gmail.com>
2025-05-30 15:44:49 +0900

    KW-540/feat: 로그인 API 요청 시, fcmToken 포함하도록 수정

--- App.js
+++ App.js
@@ -5,21 +5,21 @@ import AppNavigator from './src/navigations/AppNavigator';
 import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
-  useEffect(() => {
-    // Firebase App이 정상적으로 초기화되었는지 확인
-    // console.log('Firebase App Name:', firebase.app().name); // [DEFAULT]가 찍히면 정상
-    // FCM 토큰 받아오기
-    const getToken = async () => {
-      try {
-        const fcmToken = await messaging().getToken();
-        console.log('FCM Token:', fcmToken);
-      } catch (error) {
-        console.log('Error getting FCM token:', error);
-      }
-    };
+  // useEffect(() => {
+  //   // Firebase App이 정상적으로 초기화되었는지 확인
+  //   // console.log('Firebase App Name:', firebase.app().name); // [DEFAULT]가 찍히면 정상
+  //   // FCM 토큰 받아오기
+  //   const getToken = async () => {
+  //     try {
+  //       const fcmToken = await messaging().getToken();
+  //       console.log('FCM Token:', fcmToken);
+  //     } catch (error) {
+  //       console.log('Error getting FCM token:', error);
+  //     }
+  //   };
 
-    getToken();
-  }, []);
+  //   getToken();
+  // }, []);
   return (
     <SafeAreaProvider>
       <AppNavigator />
--- src/apis/LoginApi.js
+++ src/apis/LoginApi.js
@@ -5,6 +5,7 @@ export const loginUser = async (form) => {
   const response = await axios.post('/auth/login', {
     email: form.email,
     password: form.pw,
+    fcmToken: form.fcmToken,
   });
 
   return response.data;
