420d24e0a8d90129c3c32a782f5b5daa00eb2e4f
Jiwon Chae <63784453+jiwon0226@users.noreply.github.com>
2025-06-04 20:06:20 +0900

    KW-593/feat: 회색버튼 style prop 추가

--- src/components/buttons/GrayButton.js
+++ src/components/buttons/GrayButton.js
@@ -4,10 +4,11 @@ import { styles } from './styles/GrayButton.styles';
 const GrayButton = ({
   title, // 버튼 텍스트
   onPressHandler, // 버튼 클릭 시 핸들러 함수
+  style,
 }) => {
   return (
     <TouchableOpacity onPress={onPressHandler} style={styles.button}>
-      <Text style={styles.text}>{title}</Text>
+      <Text style={(styles.text, style)}>{title}</Text>
     </TouchableOpacity>
   );
 };
